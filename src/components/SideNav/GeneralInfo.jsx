f0dda3471cd732b4b756bfd116acb8832c9278b5
Brian Teeters <BrianTeetersDesign@gmail.com>
2025-09-25 09:47:48 -0400

    swap to general info display mode on submit click

--- src/components/SideNav/GeneralInfo.jsx
+++ src/components/SideNav/GeneralInfo.jsx
@@ -17,47 +17,47 @@ function GeneralInfo() {
         <>
             <div className="card m-1 p-3">
                 <h3>General Information</h3>
-                <form action="">
-                    <label htmlFor="">First Name</label>
-                    <input type="text" placeholder="First Name" 
-                        // The following are the parts needed to link the input to the useState Object
-                        value={general.firstName}
-                        onChange={(e) => setGeneral({...general, firstName: e.target.value })}
-                    />
+                {isEditing && (
+                    <form action="">
+                        <label htmlFor="">First Name</label>
+                        <input type="text" placeholder="First Name" 
+                            // The following are the parts needed to link the input to the useState Object
+                            value={general.firstName}
+                            onChange={(e) => setGeneral({...general, firstName: e.target.value })}
+                        />
 
-                    <label htmlFor="">Last Name</label>
-                    <input type="text" placeholder="Last Name" 
-                        value={general.lastName}
-                        onChange={(e) => setGeneral({...general, lastName: e.target.value })}
-                    />
+                        <label htmlFor="">Last Name</label>
+                        <input type="text" placeholder="Last Name" 
+                            value={general.lastName}
+                            onChange={(e) => setGeneral({...general, lastName: e.target.value })}
+                        />
 
-                    <label htmlFor="">Job Title</label>
-                    <input type="text" placeholder="Job Title" 
-                        value={general.jobTitle}
-                        onChange={(e) => setGeneral({...general, jobTitle: e.target.value })}
-                    />
+                        <label htmlFor="">Job Title</label>
+                        <input type="text" placeholder="Job Title" 
+                            value={general.jobTitle}
+                            onChange={(e) => setGeneral({...general, jobTitle: e.target.value })}
+                        />
 
-                    <label htmlFor="">Email</label>
-                    <input type="email" placeholder="Email" 
-                        value={general.email}
-                        onChange={(e) => setGeneral({...general, email: e.target.value })}
-                    />
+                        <label htmlFor="">Email</label>
+                        <input type="email" placeholder="Email" 
+                            value={general.email}
+                            onChange={(e) => setGeneral({...general, email: e.target.value })}
+                        />
 
-                    <label htmlFor="">Phone</label>
-                    <input type="tel" placeholder="Phone" 
-                        value={general.phone}
-                        onChange={(e) => setGeneral({...general, phone: e.target.value })}
-                    />
+                        <label htmlFor="">Phone</label>
+                        <input type="tel" placeholder="Phone" 
+                            value={general.phone}
+                            onChange={(e) => setGeneral({...general, phone: e.target.value })}
+                        />
 
-                    <label htmlFor="">Location</label>
-                    <input type="text" placeholder="Location" 
-                        value={general.location}
-                        onChange={(e) => setGeneral({...general, location: e.target.value })}
-                    />
+                        <label htmlFor="">Location</label>
+                        <input type="text" placeholder="Location" 
+                            value={general.location}
+                            onChange={(e) => setGeneral({...general, location: e.target.value })}
+                        />
 
-                    <button>Add Link</button>
+                        <button>Add Link</button>
 
-                    {isEditing && (
                         <button
                             // make submit button switch editing mode to "false"
                             onClick={(e) => {
@@ -65,15 +65,23 @@ function GeneralInfo() {
                                 setIsEditing(false);
                             }}
                         >Submit</button>
-                    )}
+                    </form>
+                )}
 
-                    {!isEditing && (
+                {!isEditing && (
+                    <div>
+                        <p>{general.firstName} {general.lastName}</p>
+                        <p>{general.jobTitle}</p>
+                        <p>{general.email}</p>
+                        <p>{general.phone}</p>
+                        <p>{general.location}</p>
+                        <p>{general.links}</p>
                         <button
                             // make edit button switch editing mode to "true"
                             onClick={() => setIsEditing(true)}
                         >Edit</button>
-                    )}
-                </form>
+                    </div>
+                )}
             </div>
         </>
     )
