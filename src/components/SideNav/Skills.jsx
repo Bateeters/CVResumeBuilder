7f42d83e63d21c965693444bcc8916e29c449c1e
Brian Teeters <BrianTeetersDesign@gmail.com>
2025-09-29 16:16:09 -0400

    Updated styling for Skills edit section

--- src/components/SideNav/Skills.jsx
+++ src/components/SideNav/Skills.jsx
@@ -5,22 +5,27 @@ function Skills({ skills, setSkills }) {
 
     return (
         <>
-            <div className="card m-1 mb-5 p-3">
-                <h3>Skills</h3>
+            <div className="card mx-1 mb-5 p-3 shadow-sm row justify-content-center">
+                <h3 className="p-0">Skills</h3>
+                <hr />
                 {isEditing && (
-                    <form action="">
+                    <form action="" className="row col-12 justify-content-between m-auto p-0">
                         <textarea
+                            className="col-12 mb-3"
                             placeholder="Technical (Hard) Skills"
                             value={skills.technicalSkills}
                             onChange={(e) => setSkills({...skills, technicalSkills: e.target.value})}
                             />
-                            <textarea
+                        <textarea
+                            className="col-12 mb-3"
                             placeholder="Soft Skills"
                             value={skills.softSkills}
                             onChange={(e) => setSkills({...skills, softSkills: e.target.value})}
                         />
 
                         <button
+                            className="col-12 btn btn-success"
+                            type="button"
                             // make submit button switch editing mode to "false"
                             onClick={(e) => {
                                 e.preventDefault(); // makes button stop reload/state reset
@@ -31,12 +36,14 @@ function Skills({ skills, setSkills }) {
                 )}
 
                 {!isEditing && (
-                    <div>
-                        <h5>Technical Skills</h5>
+                    <div className="p-0">
+                        <h5 className="mb-0">Technical Skills:</h5>
                         <p>{skills.technicalSkills}</p>
-                        <h5>Soft Skills</h5>
+                        <h5 className="mb-0">Soft Skills:</h5>
                         <p>{skills.softSkills}</p>
                         <button
+                            type="button"
+                            className="col-12 btn btn-primary"
                             // make edit button switch editing mode to "true"
                             onClick={() => setIsEditing(true)}
                         >Edit</button>
